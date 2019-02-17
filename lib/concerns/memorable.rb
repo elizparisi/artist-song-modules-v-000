bd557268e70974858064d6ed2b641621024445d3
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-17 04:53:25 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,6 +1,5 @@
 module Memorable
   module ClassMethods
-    
     def reset_all
       self.all.clear
     end
@@ -8,7 +7,8 @@ module Memorable
     def count
       self.all.count
     end
-    
+  end
+  
   module InstanceMethods
     def initialize
       self.class.all << self
